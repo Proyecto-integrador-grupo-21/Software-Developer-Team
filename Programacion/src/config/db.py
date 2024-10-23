4a3e7978dbbecab0236e7a7b741b7f704eddced3
miguel <miguel.scaccia1@gmail.com>
2024-10-23 20:36:47 -0300

    Mejoras de db y agregado de clase Accion

--- Programacion/src/config/db.py
+++ Programacion/src/config/db.py
@@ -1,58 +1,52 @@
 import mysql.connector
 from mysql.connector import Error
 
-# En rasgos generales esta armado la clase Conector, queda a revisar y gestionar mejoras
 class ConectorDB:
-    def __init__(self):
-        self.host = "localhost"
-        self.user = "root"
-        self.password = ""
-        self.database = "ARGBrokerDB"
-        self.conexion = None
+    def __init__(self, host="localhost", user="root", password="", database="ARGBrokerDB"):
+        self._host = host
+        self._user = user
+        self._password = password
+        self._database = database
+        self._conexion = None
 
     def conexion_db(self):
-        if self.conexion is None or not self.conexion.is_connected():
+        if self._conexion is None or not self._conexion.is_connected():
             try:
-                self.conexion = mysql.connector.connect(
-                    host=self.host,
-                    user=self.user,
-                    password=self.password,
-                    database=self.database
+                self._conexion = mysql.connector.connect(
+                    host=self._host,
+                    user=self._user,
+                    password=self._password,
+                    database=self._database
                 )
             except Error as error:
                 raise Exception(f"Error al conectarse a la base de datos: {error}")
-        return self.conexion
+        return self._conexion
 
     def desconectar(self):
-        if self.conexion and self.conexion.is_connected():
-            self.conexion.close()
+        if self._conexion and self._conexion.is_connected():
+            self._conexion.close()
 
     def ejecutar_consulta(self, consulta, parametros=None):
         conexion = self.conexion_db()
         try:
-            cursor = conexion.cursor()
-            if parametros:
-                cursor.execute(consulta, parametros)
-            else:
-                cursor.execute(consulta)
-            conexion.commit()
-            return cursor
+            with conexion.cursor() as cursor:
+                if parametros:
+                    cursor.execute(consulta, parametros)
+                else:
+                    cursor.execute(consulta)
+                conexion.commit()
         except Error as error:
             raise Exception(f"Error al ejecutar la consulta: {error}")
-        finally:
-            cursor.close()
-\
+
     def obtener_datos(self, consulta, parametros=None):
         conexion = self.conexion_db()
         try:
-            cursor = conexion.cursor()
-            if parametros:
-                cursor.execute(consulta, parametros)
-            else:
-                cursor.execute(consulta)
-            resultados = cursor.fetchall()
-            return resultados
+            with conexion.cursor() as cursor:
+                if parametros:
+                    cursor.execute(consulta, parametros)
+                else:
+                    cursor.execute(consulta)
+                resultados = cursor.fetchall()
+                return resultados
         except Error as error:
             raise Exception(f"Error al obtener los datos: {error}")
-        finally:
-            cursor.close()
--- Programacion/src/model/accion.py
+++ Programacion/src/model/accion.py
@@ -1,2 +1,70 @@
-class Accion ():
-    pass
+class Accion:
+    def __init__(self, simbolo, nombre_empresa, ultimo_precio_operado, apertura, minimo_diario, maximo_diario, ultimo_cierre, cantidad_compra_diaria, precio_compra, precio_venta, cantidad_venta_diaria):
+        self._simbolo = simbolo
+        self._nombre_empresa = nombre_empresa
+        self._ultimo_precio_operado = ultimo_precio_operado
+        self._apertura = apertura
+        self._minimo_diario = minimo_diario
+        self._maximo_diario = maximo_diario
+        self._ultimo_cierre = ultimo_cierre
+        self._cantidad_compra_diaria = cantidad_compra_diaria
+        self._precio_compra = precio_compra
+        self._precio_venta = precio_venta
+        self._cantidad_venta_diaria = cantidad_venta_diaria
+
+    @property
+    def simbolo(self):
+        return self._simbolo
+
+    @property
+    def nombre_empresa(self):
+        return self._nombre_empresa
+
+    @property
+    def ultimo_precio_operado(self):
+        return self._ultimo_precio_operado
+
+    @property
+    def apertura(self):
+        return self._apertura
+
+    @property
+    def minimo_diario(self):
+        return self._minimo_diario
+
+    @property
+    def maximo_diario(self):
+        return self._maximo_diario
+
+    @property
+    def ultimo_cierre(self):
+        return self._ultimo_cierre
+
+    @property
+    def cantidad_compra_diaria(self):
+        return self._cantidad_compra_diaria
+
+    @property
+    def precio_compra(self):
+        return self._precio_compra
+
+    @property
+    def precio_venta(self):
+        return self._precio_venta
+
+    @property
+    def cantidad_venta_diaria(self):
+        return self._cantidad_venta_diaria
+
+    def __str__(self):
+        return (f"Acción: {self.simbolo} - {self.nombre_empresa}\n"
+                f"Último Precio: {self.ultimo_precio_operado}\n"
+                f"Apertura: {self.apertura}\n"
+                f"Mínimo Diario: {self.minimo_diario}\n"
+                f"Máximo Diario: {self.maximo_diario}\n"
+                f"Último Cierre: {self.ultimo_cierre}\n"
+                f"Cantidad Compra Diaria: {self.cantidad_compra_diaria}\n"
+                f"Precio Compra: {self.precio_compra}\n"
+                f"Precio Venta: {self.precio_venta}\n"
+                f"Cantidad Venta Diaria: {self.cantidad_venta_diaria}")
+
